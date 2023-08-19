3da8b3ecd7fb7f1cdac8e8c1592c8a3bf3d2adeb
debbie-herridge <debbie.herridge@icloud.com>
2023-08-19 15:53:25 +0000

    restart code from third commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn bee-inkspired.wsgi
--- eny.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://rnjveynw:ba8mzvxE27SuHO_6r4NWiy1RnBZlmIYi@rogue.db.elephantsql.com/rnjveynw"
-os.environ["SECRET_KEY"] = "s_uper@key"
