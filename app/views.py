999d85c20a58f43b691695305374cfd905ba7b38
debbie-herridge <debbie.herridge@icloud.com>
2023-08-20 19:32:23 +0000

    add map to contact page

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
 
 def gallery(request):
     return render(request, 'gallery.html')
