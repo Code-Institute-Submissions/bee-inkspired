9c7d3a1cb9280e4115a27b3fea2efb02aa9b606c
debbie-herridge <debbie.herridge@icloud.com>
2023-08-20 12:20:24 +0000

    nav and footer on base.html with working links

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('artist/', views.artist, name='artist'),
+    path('contact/', views.contact, name='contact'),
+    path('book', views.book, name='book'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,16 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def artist(request):
+    return render(request, 'artist.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def book(request):
+    return render(request, 'book.html')
+
