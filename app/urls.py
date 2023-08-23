b07269302ac7601207467c1ff2e76b6a7afc2cef
debbie-herridge <debbie.herridge@icloud.com>
2023-08-23 20:52:15 +0000

    styling to login and register page

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
 
     path('login/', views.loginPage, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
 
     path('dashboard/<str:pk>/', views.client, name="customer"),
     path('dashboard-artist/', views.dashboard, name='dashboard'),
